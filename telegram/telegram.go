51a52233adf8531241c22e8eb0ff6f217b297647
Bach Nguyen <bachnv@teca.vn>
2026-02-08 10:05:32 +0700

    refactor: greeting

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -268,7 +268,7 @@ func (t *Telegram) handStartCommand(chat Chat) {
 	if err := t.SendMessage(SendMessageRequest{
 		ChatID:    chat.ID,
 		ParseMode: "HTML",
-		Text:      fmt.Sprintf("<b>🎉🎉🎉 Chào mừng %s %s đến với 🐶 Cậu Vàng!</b>", chat.LastName, chat.FirstName),
+		Text:      fmt.Sprintf("<b>🎉🎉🎉\n\nChào mừng %s %s đến với 🐶 Cậu Vàng!</b>", chat.LastName, chat.FirstName),
 	}); err != nil {
 		log.Error().Err(err).Send()
 	}
